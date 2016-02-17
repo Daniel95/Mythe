ec0a7501ea23d0fdaef213d8771c49fa188a7ad1
nathan-san <18964@ma-web.nl>
2016-02-17 13:49:04 +0100

    merged with sebas branch.

---
+++ Assets/Scripts/LoopingBackground/BackgroundLooper.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundLooper : MonoBehaviour {
+
+	[SerializeField]
+	private float scrollingSpeed;
+
+	private Renderer bgRenderer;
+
+	void Start () {
+		bgRenderer = GetComponent<Renderer> ();
+	}
+
+	void Update () {
+		//Sets the offset of the texture to give the illusion of looping
+		Vector2 offset = new Vector2 (0,Time.time*scrollingSpeed);
+		bgRenderer.material.mainTextureOffset = offset;﻿
+	}
+}

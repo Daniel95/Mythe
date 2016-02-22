3fb24c01b218e2e5d318e43423d0ae6dc716ed58
ezowolf <wiggler@live.nl>
2016-02-22 11:20:40 +0100

    Updated background

--- Assets/Scripts/LoopingBackground/BackgroundLooper.cs
+++ Assets/Scripts/LoopingBackground/BackgroundLooper.cs
@@ -1,10 +1,14 @@
 ﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class BackgroundLooper : MonoBehaviour {
 
 	[SerializeField]
 	private float scrollingSpeed;
+
+	[SerializeField]
+	private Material skyMaterial;
 
 	private Renderer bgRenderer;
 
@@ -16,5 +20,14 @@ public class BackgroundLooper : MonoBehaviour {
 		//Sets the offset of the texture to give the illusion of looping
 		Vector2 offset = new Vector2 (0,Time.time*scrollingSpeed);
 		bgRenderer.material.mainTextureOffset = offset;﻿
+
+
+		if (Input.GetKey (KeyCode.Space)) {
+			ChangeMaterial (skyMaterial);
+		}
+	}
+	public void ChangeMaterial(Material newMaterial)
+	{
+		bgRenderer.material = newMaterial;
 	}
 }

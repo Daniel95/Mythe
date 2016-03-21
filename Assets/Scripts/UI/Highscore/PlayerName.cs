0a42c2ccbc0a073320be48710bd752b0ea2c9126
RivervdMeulen <River@Rivers-MacBook-Pro.local>
2016-03-21 14:00:07 +0100

    Revert "Empty Branch"
    This reverts commit 34ae3ba36cb74f891ec5c6afd031effcda58ea35.

---
+++ Assets/Scripts/UI/Highscore/PlayerName.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class PlayerName : MonoBehaviour
+{
+    private string _playerName;
+
+    public string Name
+    {
+        get { return _playerName; }
+        set { _playerName = value; }
+    }
+}
---
+++ Assets/Scripts/UI/UIStateChangers/FinishGame.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class FinishGame : MonoBehaviour {
+
+    [SerializeField]
+    private List<ScoreBase> scores;
+
+    [SerializeField]
+    private ScoreBoard scoreBoard;
+
+    [SerializeField]
+    private GameObject restartButton;
+
+    public void Finish() {
+
+        scoreBoard.gameObject.SetActive(true);
+        scoreBoard.Finished();
+
+        foreach (ScoreBase _score in scores) {
+            _score.Counting = false;
+        }
+
+        restartButton.SetActive(true);
+        gameObject.SetActive(false);
+    }
+}

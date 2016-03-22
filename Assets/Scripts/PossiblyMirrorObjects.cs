475b84f4cc852985f2316e8d1ea0a8a1fcc9b26f
ezowolf <wiggler@live.nl>
2016-03-22 14:05:18 +0100

    Possibly mirror trees

---
+++ Assets/Scripts/PossiblyMirrorObjects.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PossiblyMirrorObjects : MonoBehaviour {
+
+	void OnEnable()
+	{
+		if (Random.value < 0.5)
+		{
+			Vector3 theScale = transform.localScale;
+			theScale.x *= -1;
+			transform.localScale = theScale;
+			Debug.Log ("FLIP");
+		}
+	}
+}

eb60edb5dd1a0a4bbf5b7951c0a41567962dd6b9
nathan-san <18964@ma-web.nl>
2016-03-30 14:36:45 +0200

    chunkUnlocker script done (only added one chunk)

--- Assets/Scripts/Generator/ChunkUnlocker.cs
+++ Assets/Scripts/Generator/ChunkUnlocker.cs
@@ -1,6 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
-
+using System.Collections;
 public class ChunkUnlocker : MonoBehaviour {
 
 	[SerializeField]
@@ -8,36 +8,90 @@ public class ChunkUnlocker : MonoBehaviour {
 
 	[SerializeField]
 	private PlayerDistance playerDistance;
-	public List<string> notUnlockedChunks;
 
 	[SerializeField]
 	private List<int> unlockValues;
 
-	[SerializeField]
-	private List<int> ExtraChunks;
+    [SerializeField]
+    private List<string> introChunks;
 
+    [SerializeField]
+    private List<string> easyChunks;
 
+    [SerializeField]
+    private List<string> mediumChunks;
+
+    [SerializeField]
+    private List<string> hardChunks;
+
+    [SerializeField]
+    private List<string> superHardChunks;
+
+    [SerializeField]
+    private List<string> extremeChunks;
+
+    void Start()
+    {
+        StartCoroutine(Unlocking());
+    }
 	public void UnlockChunk(string chunkToUnlock)
 	{
 		//Use this to force unlock new chunks.
 		chunkHolder.LoadChunk (chunkToUnlock);
 	}
+    IEnumerator Unlocking()
+    {
+        for(int i = 0; i<introChunks.Count; i++)
+        {
+            UnlockChunk(introChunks[i]);
+        }
 
-	public void UnlockChunksWithDistance()
-	{
-		//This function is called constantly and will unlock new chunks based on score.
-		int distance = playerDistance.Distance;
-		for(int i = 0; i<unlockValues.Count; i++) 
-		{
-			if (distance >= unlockValues[i]) 
-			{
-				//Removes the unlock values based on distance.
-				unlockValues.Remove (unlockValues[i]);
-				notUnlockedChunks.Remove (notUnlockedChunks [i]);
-				//Then unlocks 
-				UnlockChunk (notUnlockedChunks[i]);
+        while (playerDistance.Distance < 1000f)
+        {
+            yield return new WaitForFixedUpdate();
+        }
+        for (int i = 0; i < easyChunks.Count; i++)
+        {
+            UnlockChunk(easyChunks[i]);
+        }
 
-			}
-		}
-	}
+        while (playerDistance.Distance < 2000f)
+        {
+            yield return new WaitForFixedUpdate();
+        }
+        for (int i = 0; i < mediumChunks.Count; i++)
+        {
+            UnlockChunk(mediumChunks[i]);
+        }
+
+        while (playerDistance.Distance < 3000f)
+        {
+            yield return new WaitForFixedUpdate();
+        }
+        for (int i = 0; i < hardChunks.Count; i++)
+        {
+            UnlockChunk(hardChunks[i]);
+        }
+
+        while (playerDistance.Distance < 4000f)
+        {
+            yield return new WaitForFixedUpdate();
+        }
+        for (int i = 0; i < superHardChunks.Count; i++)
+        {
+            UnlockChunk(superHardChunks[i]);
+        }
+
+        while (playerDistance.Distance < 5000f)
+        {
+            yield return new WaitForFixedUpdate();
+        }
+        for (int i = 0; i <extremeChunks.Count; i++)
+        {
+            UnlockChunk(extremeChunks[i]);
+        }
+
+
+
+    }
 }

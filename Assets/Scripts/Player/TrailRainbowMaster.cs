d0e8eb0006d0e1b1e91ddfdb53f03f49cd4045cb
RivervdMeulen <River@Rivers-MacBook-Pro.local>
2016-03-30 13:19:53 +0200

    New trail scripts added

---
+++ Assets/Scripts/Player/TrailRainbowMaster.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrailRainbowMaster : MonoBehaviour {
+
+	public float trail_red = 1;
+	public float trail_green = 0;
+	public float trail_blue = 0;
+
+	[SerializeField]
+	private float fading = 0.02f;
+
+	public void getColor () {
+		if(trail_red >= 1 && trail_green < 1 && trail_blue <= 0){
+			trail_green += fading;
+		} else if(trail_red >= 0 && trail_green >= 1 && trail_blue <= 0){
+			trail_red -= fading;
+		} else if(trail_red <= 0 && trail_green >= 1 && trail_blue < 1){
+			trail_blue += fading;
+		} else if(trail_red <= 0 && trail_green >= 0 && trail_blue >= 1){
+			trail_green -= fading;
+		} else if(trail_red < 1 && trail_green <= 0 && trail_blue >= 1){
+			trail_red += fading;
+		} else if(trail_red >= 1 && trail_green <= 0 && trail_blue >= 0){
+			trail_blue -= fading;
+		}
+	}
+}

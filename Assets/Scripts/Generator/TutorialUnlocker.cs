58b95b2ddaf0d2288cc0c3e0a5b581d2c9b91e86
nathan-san <18964@ma-web.nl>
2016-03-30 10:30:20 +0200

    added credits art and ground effect active in game

---
+++ Assets/Scripts/Generator/TutorialUnlocker.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TutorialUnlocker : MonoBehaviour {
+
+    [SerializeField]
+    private ChunkHolder chunkHolder;
+    [SerializeField]
+    private GenerateChunk generateChunk;
+    [SerializeField]
+    private PlayerDistance playerDistance;
+    void Awake()
+    {
+        
+    }
+    void Start () {
+        StartCoroutine(ForceIntroChunks());
+	}
+	IEnumerator ForceIntroChunks()
+    {
+        //intro game with two rows of obstacles and some water.
+        generateChunk.MakeChosenChunk(chunkHolder.UncompressChunk("100001100001110301100001100001100001100001100001103011100001100001100001"));
+
+        //intro chunk for super modus.
+        while (playerDistance.Distance < 1000)
+        {
+            yield return new WaitForFixedUpdate();
+        }
+        generateChunk.MakeChosenChunk(chunkHolder.UncompressChunk("111111111111111111113311133331333333333333333333333333333333000000000000"));
+
+
+        //intro chunk for shield pick up.
+        while (playerDistance.Distance < 2000)
+        {
+            yield return new WaitForFixedUpdate();
+        }
+        generateChunk.MakeChosenChunk(chunkHolder.UncompressChunk("000000000000111111111111000000005500000000010010100001"));
+
+
+        //intro chunk for magnet pick up up.
+        while (playerDistance.Distance < 3000)
+        {
+            yield return new WaitForFixedUpdate();
+        }
+        generateChunk.MakeChosenChunk(chunkHolder.UncompressChunk("000000000000110000310000110011000013000011000000000000004000000000000000"));
+
+
+        //intro chunk for side walk enemies.
+        while (playerDistance.Distance < 4000)
+        {
+            yield return new WaitForFixedUpdate();
+        }
+        generateChunk.MakeChosenChunk(chunkHolder.UncompressChunk("000000000300000000000000000007000000000000000300000000000000700000000000000000"));
+
+
+        //intro chunk for iaming enemies.
+        while (playerDistance.Distance < 5000)
+        {
+            yield return new WaitForFixedUpdate();
+        }
+        generateChunk.MakeChosenChunk(chunkHolder.UncompressChunk("000000000000000000000000000030000000060003000000000030000000000000000000000000"));
+
+
+    }
+}

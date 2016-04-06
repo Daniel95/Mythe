2e7cacafa978a0a3ef242aefee9c5fcb6b0cd380
ezowolf <wiggler@live.nl>
2016-04-06 17:05:56 +0200

    OPTIONS FINALLY WORK

---
+++ Assets/Scripts/UI/SavingAndLoading/OptionButtonCorrecter.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OptionButtonCorrecter : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject objectToEnableCursor;
+	[SerializeField]
+	private GameObject objectToDisableCursor;
+
+	[SerializeField]
+	private GameObject objectToEnableVibration;
+	[SerializeField]
+	private GameObject objectToDisableVibration;
+
+	[SerializeField]
+	private GameObject objectToEnableTutorial;
+	[SerializeField]
+	private GameObject objectToDisableTutorial;
+
+	private OptionsData optionsData;
+
+	void Start()
+	{
+		if (GameObject.FindGameObjectWithTag ("Data")) {
+			optionsData = GameObject.FindGameObjectWithTag ("Data").GetComponent<OptionsData> ();
+		} else 
+		{
+			Debug.LogError ("No options data found. Did you start from the correct scene?");
+		}
+		Debug.Log (optionsData.GetTutorial);
+		if (optionsData.GetCursor==false) 
+		{
+			objectToDisableCursor.SetActive (false);
+			objectToEnableCursor.SetActive (true);
+		}
+		if (optionsData.GetVibration==false) 
+		{
+			objectToDisableVibration.SetActive (false);
+			objectToEnableVibration.SetActive (true);
+		}
+		if (optionsData.GetTutorial==false) 
+		{
+			objectToDisableTutorial.SetActive (false);
+			objectToEnableTutorial.SetActive (true);
+		}
+			
+	}
+
+
+}
---
+++ Assets/Scripts/UI/SavingAndLoading/SetChangeTutorialSetting.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SetChangeTutorialSetting : MonoBehaviour {
+
+	public void ChangeTutorialSetting(bool _setting) 
+	{
+		if (GameObject.FindGameObjectWithTag ("Data"))
+			GameObject.FindGameObjectWithTag ("Data").GetComponent<OptionsData> ().PlayTutorial = _setting;
+	}
+}

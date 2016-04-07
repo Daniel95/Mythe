792fc835e6c0eaa7ea59d192b316f54063460941
ezowolf <wiggler@live.nl>
2016-04-07 13:23:53 +0200

    Random Obstacle Prefab

---
+++ Assets/RandomObstaclePooler.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomObstaclePooler : MonoBehaviour {
+
+	[SerializeField]
+	private string[] objectToSpawnNames;
+
+	private bool trueActive = false;
+	void OnEnable()
+	{
+		
+		if (trueActive) {
+			GameObject spawnedObject = ObjectPool.instance.GetObjectForType (objectToSpawnNames [Random.Range (0, objectToSpawnNames.Length)], true);
+			spawnedObject.transform.position = this.transform.position;
+			ObjectPool.instance.PoolObject (gameObject);
+		} else {
+			trueActive = true;
+		}
+	}
+}
